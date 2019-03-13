06b066d5b5e941ffedc1943ae25539084810be5d
Jeremy Boggs <nicocchilover@gmail.com>
2019-03-12 20:41:20 -0400

    redux boilerplate setup

---
+++ client/src/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+// Bootstrap, MU, fonts, other style libraries
+
+
+// Router
+import { BrowserRouter as Router } from "react-router-dom";
+
+// Redux
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+import * as serviceWorker from "./serviceWorker";
+
+import rootReducer from "./store/reducers/";
+
+// Redux dev tools
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk, logger),
+    // other store enhancers if any
+);
+const store = createStore(rootReducer, enhancer);
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+    rootElement
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
